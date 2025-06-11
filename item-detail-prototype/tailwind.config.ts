f74ad31fbecc01d089ba12b26653a4bdd0a50669
wgcostta <wg.o.costa@gmail.com>
2025-06-11 13:49:58 -0300

    feat: correções do layout

--- item-detail-prototype/tailwind.config.ts
+++ item-detail-prototype/tailwind.config.ts
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-theme: {
-  extend: {
-    fontFamily: {
-      sans: ['Roboto', 'ui-sans-serif', 'system-ui', 'sans-serif'],
-    },
+  theme: {
+    extend: {},
   },
-},
   plugins: [],
-};
+}
