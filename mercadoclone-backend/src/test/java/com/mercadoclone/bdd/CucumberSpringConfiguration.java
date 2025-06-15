d07713d5c4dc8fcc1c360b2ab0360ae61f464e74
wgcostta <wg.o.costa@gmail.com>
2025-06-15 09:29:05 -0300

    feat: implementação da cobertura de testes no backend

--- mercadoclone-backend/src/test/java/com/mercadoclone/bdd/CucumberSpringConfiguration.java
+++ mercadoclone-backend/src/test/java/com/mercadoclone/bdd/CucumberSpringConfiguration.java
@@ -1,6 +1,3 @@
-/**
- * Configuração Spring para Cucumber.
- */
 package com.mercadoclone.bdd;
 
 import com.mercadoclone.MeliCloneApplication;
@@ -10,10 +7,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 
-/**
- * Configuração base que garante que o Spring Context seja carregado
- * corretamente para os testes Cucumber.
- */
 @CucumberContextConfiguration
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
@@ -27,9 +20,6 @@ public class CucumberSpringConfiguration {
 
     protected TestRestTemplate restTemplate = new TestRestTemplate();
 
-    /**
-     * Retorna a URL base para os testes.
-     */
     protected String getBaseUrl() {
         return "http://localhost:" + port;
     }
